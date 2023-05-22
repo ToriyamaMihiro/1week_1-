9c7c78c14e7edc419c6c2fd71f60a3ab12644c95
YosidaMei <k022g0007@g.neec.ac.jp>
2023-05-22 23:16:30 +0900

    敵の弾発射

--- Assets/Enemy/HandL.cs
+++ Assets/Enemy/HandL.cs
@@ -7,12 +7,15 @@ public class HandL : MonoBehaviour
     public float hand_speed = 0.1f;
     public float cricle_radius = 5.0f;
     private Vector3 initPosition;
+    public float fire_frame = 0;
+    Vector3 bullet_pos;//’e‚ÌˆÊ’u
 
     // Start is called before the first frame update
     void Start()
     {
         transform.position = new Vector3(-10,0 , 0);
         initPosition = transform.position;
+        bullet_pos = transform.Find("BulletPosition").localPosition;
     }
 
     void Circle()
@@ -26,6 +29,8 @@ public class HandL : MonoBehaviour
         l_pos.y = Mathf.Sin(rad) * cricle_radius;
 
         transform.position = l_pos;
+
+
     }
 
     // Update is called once per frame
@@ -34,6 +39,16 @@ public class HandL : MonoBehaviour
 
         Circle();
 
+        //’e‚Ì”­ŽË
+        fire_frame++;
+        if (fire_frame % 10)//10•b‚²‚Æ‚É”­ŽË
+        {
+            Instantiate(BulletPrefab, transform.position + bullet_pos, Quaternion.identity);
+        }
+        if (fire_frame >= 1000)
+        {
+            fire_frame = 0;
+        }
 
     }
 }
--- Assets/Enemy/HandScript.cs
+++ Assets/Enemy/HandScript.cs
@@ -8,12 +8,15 @@ public class HandScript : MonoBehaviour
     public float hand_speed = 0.1f;
     public float cricle_radius = 5.0f;
     private Vector3 initPosition;
+    public float fire_frame = 0;
+    Vector3 bullet_pos;//’e‚ÌˆÊ’u
 
     // Start is called before the first frame update
     void Start()
     {
         transform.position = new Vector3(10, 0, 0);
         initPosition = transform.position;
+        bullet_pos = transform.Find("BulletPosition").localPosition;
     }
 
     void Circle()
@@ -35,7 +38,17 @@ public class HandScript : MonoBehaviour
     {
 
         Circle();
-     
+
+        //’e‚Ì”­ŽË
+        fire_frame++;
+        if (fire_frame % 10)//10•b‚²‚Æ‚É”­ŽË
+        {
+            Instantiate(BulletPrefab, transform.position + bullet_pos, Quaternion.identity);
+        }
+        if (fire_frame >= 1000)
+        {
+            fire_frame = 0;
+        }
 
 
     }
