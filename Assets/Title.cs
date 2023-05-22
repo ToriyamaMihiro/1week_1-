15207f5fb7536a5c5b6a19150a154f9d7fc256f4
YosidaMei <k022g0007@g.neec.ac.jp>
2023-05-22 14:26:12 +0900

    space押すとシーンが変わる

--- Assets/Title.cs
+++ Assets/Title.cs
@@ -7,12 +7,16 @@ public class Title : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Invoke("ChangeScene",3.0f);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+
     }
 }
