fb694ed6bcbc7c6e43e09711bbfc1027ce7b001e
YosidaMei <k022g0007@g.neec.ac.jp>
2023-05-22 16:15:13 +0900

    エネミーのHPを実装

--- Assets/Enemy/EnemyScript.cs
+++ Assets/Enemy/EnemyScript.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
+    //変数
+    int maxHp = 10;
+    int hp;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        hp = maxHp;
     }
 
     // Update is called once per frame
@@ -19,11 +23,14 @@ public class EnemyScript : MonoBehaviour
         // 当たったのがプレイヤーの弾
         if (other.gameObject.CompareTag("PlayerBullet"))
         {
-            // 自身を消す
-            Destroy(gameObject);
-
+            hp--;
             // 弾も消す
             Destroy(other.gameObject);
+        }
+        if (hp <= 0)
+        {
+            // 自身を消す
+            Destroy(gameObject);
         }
     }
 }
