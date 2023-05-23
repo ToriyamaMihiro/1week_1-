1bd55e333d5d4ed18516222d7af68849b6510e2a
ToriyamaMihiro <k022g0003@g.neec.ac.jp>
2023-05-24 03:50:36 +0900

    ゲームオーバーのシーン切り替え

--- Assets/Scenes/GameClear.cs
+++ Assets/Scenes/GameClear.cs
@@ -14,7 +14,7 @@ public class GameClear : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetButton("Fire1"))
         {
             SceneManager.LoadScene("Title");
         }
